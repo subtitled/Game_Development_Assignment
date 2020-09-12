4f71bc1277c62403a3e071ea2720c97356e6ae0a
Thomas King <thomas.king@griffithuni.edu.au>
2020-09-12 10:53:25 +1000

    added characters for testing

---
+++ Assets/Scripts/classScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class classScript : MonoBehaviour
+{
+    public int wep_1 = 0;
+    public int wep_2 = 0;
+    public int abil_1 = 0;
+    public int abil_2 = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void primaryWeapon()
+    {
+        switch (wep_1)
+        {
+            case 2:
+                //Enter code to set this weapon
+                break;
+            case 1:
+                //Enter code to set this weapon
+                break;
+            default:
+                //Enter code to set this weapon
+                break;
+        }
+    }
+}
