f1e5ff080f3e3bc20146d0d38b40922f92af4d06
Thomas King <thomas.king@griffithuni.edu.au>
2020-09-17 16:08:10 +1000

    added a but more UI and set up C# file for the buttons in UI

---
+++ Assets/Scripts/InGameUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameSelection: MonoBehavior {
+
+    //code for moving character after clicking move button
+    public void MoveChar()
+    {
+
+    }
+    //code for defending after clicking button
+    public void Defend()
+    {
+
+    }
+    //code for using first weapon after clicking button
+    public void UseWepOne()
+    {
+
+    }
+    //code for using second weapon after clicking button
+    public void UseWepTwo()
+    {
+
+    }
+    //code for using ability one after clicking button
+    public void AbilityOne()
+    {
+
+    }
+    //code for using ability two after clicking button
+    public void AbilityTwo()
+    {
+
+    }
+}
