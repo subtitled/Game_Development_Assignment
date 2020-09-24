9a1a19cf7caa58c0ae4442fdb1524df3391d5e3d
Thomas King <thomas.king@griffithuni.edu.au>
2020-09-24 16:39:32 +1000

    playing around with spawning units from team

---
+++ Assets/Scripts/clickSpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickSpawn : MonoBehaviour
+{
+    Ray myRay;
+    RaycastHit hit;
+    public GameObject charModel;
+    public List<int> team;
+
+    void Start()
+    {
+        //adds class choices to list for spawning later
+        team.Add(PlayerPrefs.GetInt("UnitOne"));
+        team.Add(PlayerPrefs.GetInt("UnitTwo"));
+        team.Add(PlayerPrefs.GetInt("UnitThree"));
+        team.Add(PlayerPrefs.GetInt("UnitFour"));
+        team.Add(PlayerPrefs.GetInt("UnitFive"));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //CHECK AMMO SCRIPT FOR IDEA HOW TO SPAWN LIMITED AMOUNT OF UNITS
+        myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        int i = 0;
+        while (i < 5)
+        {
+
+            if (Physics.Raycast(myRay, out hit))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Instantiate(charModel, hit.point, Quaternion.identity);
+                }
+            }
+            i++;
+        }
+    }
+
+}
