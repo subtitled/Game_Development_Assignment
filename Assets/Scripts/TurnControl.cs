2c3510dfedbd412badd499063afcf4c427428834
GUGCGennC <colin.genn@griffithuni.edu.au>
2020-09-25 02:01:34 +1000

    enemyFSM and TurnControl scripts.

---
+++ Assets/Scripts/TurnControl.cs
@@ -0,0 +1,347 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnControl : MonoBehaviour
+{
+    //Turn Control FSM AI
+    public enum FSMturn
+    {
+        None,
+        Wait,
+        Activate,
+        EndTurn,
+        Victory,
+        Defeat
+    }
+    
+    public FSMturn currState;
+    
+    // Import Map-based values.
+    public GameObject[] mapObjects;
+    public GameObject[] spawnPoints;
+    // public DATA_TYPE navigationMechanism;
+    private int turnCount = 0;
+    
+    // Experience Point collectors.
+    private int generalXP = 0;
+    private int class1XP = 0;
+    private int class2XP = 0;
+    private int class3XP = 0;
+    private int class4XP = 0;
+    private int class5XP = 0;
+    
+    // Character listings.
+    private GameObject[] enemyCharacters;
+    private GameObject[] playerCharacters;
+    private List<GameObject> initiativeOrder;
+    private bool activeCharacter = false;
+    private int characterOrder = 0;
+
+    private int playersSlain = 0;
+    private int enemiesSlain = 0;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyCharacters = GameObject.FindGameObjectsWithTag("Enemy");
+        playerCharacters = GameObject.FindGameObjectsWithTag("Player");
+        HashSet<int> usedSpawns = new HashSet<int>();
+        
+        //For random placement of objects at across designated points.
+        foreach (var placeable in mapObjects)
+        {
+            while(true)
+            {
+                int spawnPosition = Random.Range(0, spawnPoints.Length);
+                if (!usedSpawns.Contains(spawnPosition))
+                {
+                    placeable.transform.position = spawnPoints[spawnPosition].transform.position;
+                    usedSpawns.Add(spawnPosition);
+                    break;
+                }
+            }
+        } 
+        
+        
+        foreach (var character in enemyCharacters)
+        {
+            // Get and randomise initiative, add to initiativeOrder.
+            character.gameObject.GetComponent<enemyFSM>().turnInitiative = 
+                character.gameObject.GetComponent<enemyFSM>().baseInitiative * Random.Range(0.7f, 1.3f); 
+            initiativeOrder.Add(character);
+            
+            // For random placement of enemies across designated points.
+            while(true)
+            {
+                int spawnPosition = Random.Range(0, spawnPoints.Length);
+                if (!usedSpawns.Contains(spawnPosition))
+                {
+                    character.transform.position = spawnPoints[spawnPosition].transform.position;
+                    usedSpawns.Add(spawnPosition);
+                    break;
+                }
+            }
+        }
+
+        foreach (var character in playerCharacters)
+        { 
+            // Need to add the initiative to the classScript, or from a common basic values script.
+            
+            // Get and randomise initiative, add to initiativeOrder.
+            character.gameObject.GetComponent<classScript>().turnInitiative = 
+                character.gameObject.GetComponent<classScript>().baseInitiative * Random.Range(0.7f, 1.3f);
+            initiativeOrder.Add(character);
+            
+        }
+        
+        // Sort initiativeOrder by initiative values
+        // Can only compare from specified components,
+        // Either use an if() to differentiate by tag, or have a common script containing variable.
+        
+        if (initiativeOrder.Count > 0)
+        {
+            initiativeOrder.Sort(delegate(GameObject o, GameObject o1)
+            {
+                return (o.GetComponent<enemyFSM>().turnInitiative).CompareTo(o1.GetComponent<enemyFSM>()
+                    .turnInitiative);
+            });
+        }
+        
+        
+        /*
+         * Player Character Placement Phase
+         * Highly UI related.
+         * Maybe just make these in a fixed position?
+        */
+        
+        turnCount = 1;
+        currState = FSMturn.Wait;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currState)
+        {
+            case FSMturn.Wait: UpdateWaitState(); break;
+            case FSMturn.Activate: UpdateActivateState(); break;
+            case FSMturn.EndTurn: UpdateEndTurnState(); break;
+            case FSMturn.Victory: UpdateVictoryState(); break;
+            case FSMturn.Defeat: UpdateDefeatState(); break;
+
+        }
+        /*
+         *FSM
+         *
+         * if activeCharacter == true
+         *     waitFSM
+         * if activeCharacter == false & characterOrder < initiativeOrder.Length
+         *     activateFSM
+         *         send activation message
+         *         characterOrder += 1
+         *         activeCharacter = true
+         * if characterOrder == initiativeOrder.Length
+         *     endturnFSM
+         *         characterOrder = 0
+         *         turnCount += 1
+         *
+         * Forced states, may need to put at start?
+         *     victoryFSM
+         *     defeatFSM
+         * 
+         * reciever function to flip activeCharacter from true to false
+         * receiver function to add dead characters to a list
+         *     function checks if all of one side are in the list
+         *     force state change to victory/defeat if true.
+         */
+    }
+
+    protected void UpdateWaitState()
+    {
+        if (activeCharacter == false)
+        {
+            if (characterOrder >= initiativeOrder.Count)
+            {
+                currState = FSMturn.EndTurn;
+            }
+            
+            currState = FSMturn.Activate;
+        }
+        
+    }
+
+    protected void UpdateActivateState()
+    {
+        if (activeCharacter == true)
+        {
+            currState = FSMturn.Wait;
+        }
+        
+        if (activeCharacter == false)
+        {
+            if (characterOrder >= initiativeOrder.Count)
+            {
+                currState = FSMturn.EndTurn;
+            }
+
+            if (characterOrder < initiativeOrder.Count)
+            {
+                initiativeOrder[characterOrder].SendMessage("ApplyActiveStatus", true);
+                activeCharacter = true;
+                characterOrder += 1;
+                currState = FSMturn.Wait;
+            }
+            
+        }
+        
+    }
+
+    protected void UpdateEndTurnState()
+    {
+        if (activeCharacter == true)
+        {
+            currState = FSMturn.Wait;
+        }
+        
+        characterOrder = 0;
+        turnCount += 1;
+        
+        // UI End Turn and Turn Start Effects.
+        
+    }
+
+    protected void UpdateVictoryState()
+    {
+        // Victory UI effects
+        
+
+        // Bonus XP for victory?
+        generalXP += 300;
+        
+        // Method of determining which classes are present, and dividing only among them?
+        class1XP += generalXP / 5;
+        class2XP += generalXP / 5;
+        class3XP += generalXP / 5;
+        class4XP += generalXP / 5;
+        class5XP += generalXP / 5;
+
+    }
+
+    protected void UpdateDefeatState()
+    {
+        // Defeat UI effects
+        
+        
+        // No bonus XP?
+        // Method of determining which classes are present, and dividing only among them?
+        class1XP += generalXP / 5;
+        class2XP += generalXP / 5;
+        class3XP += generalXP / 5;
+        class4XP += generalXP / 5;
+        class5XP += generalXP / 5;
+    }
+
+    void ApplyGeneralXP(int addXP)
+    {
+        generalXP += addXP;
+    }
+    
+    void ApplyClassXP(int classNo, int addXP)
+    // SendMessage can only contain 1 variable, but it can be a list/array...
+    {
+        switch (classNo)
+        {
+            case 1: class1XP += addXP; break;
+            case 2: class2XP += addXP; break;
+            case 3: class3XP += addXP; break;
+            case 4: class4XP += addXP; break;
+            case 5: class5XP += addXP; break;
+        }
+    }
+    
+    void ApplyClass1XP(int addXP)
+    {
+        class1XP += addXP;
+    }
+
+    void ApplyClass2XP(int addXP)
+    {
+        class2XP += addXP;
+    }
+
+    void ApplyClass3XP(int addXP)
+    {
+        class3XP += addXP;
+    }
+
+    void ApplyClass4XP(int addXP)
+    {
+        class4XP += addXP;
+    }
+
+    void ApplyClass5XP(int addXP)
+    {
+        class5XP += addXP;
+    }
+
+    void ApplyActiveStatus(bool message)
+    {
+        activeCharacter = message;
+    }
+    
+    void ApplyPlayerSlain()
+    {
+        playersSlain += 1;
+        if (playersSlain >= playerCharacters.Length)
+        {
+            currState = FSMturn.Defeat;
+        }
+            
+    }
+    
+    void ApplyEnemySlain()
+    {
+        enemiesSlain += 1;
+        if (enemiesSlain >= enemyCharacters.Length)
+        {
+            currState = FSMturn.Victory;
+        }
+    }
+    
+    
+
+/*
+Pseudocoding:
+
+Map level controller:
+START
+    Transition in from SETUP
+    Obtain waypoints
+    Obtain random features
+    Obtain creatures, create list
+    Obtain players, create list
+    Place random features
+    Place creatures
+    Determine initiative of each (value +/- randomiser)
+    Order the initiative list
+    Turn = 0
+    Enable player placement in predetermined area
+    On placement finish, enter turn mode
+TURN
+    Turn += 1
+    For character (Active creature/player) in initiative:
+        PLAYER/CREATURE ACTIONS
+        Receive messages from killed characters, store in killed list, experience tally += value
+            If no more live creatures - Victory
+            If no more live players - Defeat
+    Clean up, remove killed characters from initiative list
+    Next turn 
+END
+    Message out total experience, class experience
+    Transition out to RESULTS
+ 
+*/
+}
---
+++ Assets/Scripts/enemyFSM.cs
@@ -0,0 +1,250 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class enemyFSM : MonoBehaviour
+{
+    public enum FSMstate
+    {
+        None,
+        Wait,
+        Moving,
+        Idle,
+        Alert,
+        Engaged,
+        Dead
+    }
+
+    public FSMstate currState;
+    
+    public int baseInitiative = 10;
+    public float turnInitiative;
+
+    public int maxHealth;
+    private int currHealth;
+    
+    private bool active = false;
+    private bool dead = false;
+    private bool action = false;
+    private bool move = false;
+
+    public float alertRange = 50.0f;
+    public float engagedRange = 35.0f;
+    //public float movementRange = 10.0f;
+    //public float moveSpeed = 12.0f;
+    //public float rotSpeed = 4.0f;
+
+    private GameObject mapControl;
+    private GameObject[] playerCharacters;
+
+    private NavMeshAgent nav;
+    private Vector3 destination;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currHealth = maxHealth;
+        nav = GetComponent<NavMeshAgent>();
+        mapControl = GameObject.Find("MapControl");
+        playerCharacters = GameObject.FindGameObjectsWithTag("Player");
+        currState = FSMstate.Wait;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         * FSM
+         * if active == false
+         *     waitFSM
+         * if active == true
+         *     if dead == true
+         *        deadFSM
+         *        send message end turn
+         *     if conditions
+         *        activeFSM
+         * 
+         */
+        
+        switch (currState)
+        {
+            case FSMstate.Wait: UpdateWaitStatus(); break;
+            case FSMstate.Moving: UpdateMovingStatus(); break;
+            case FSMstate.Idle: UpdateIdleStatus(); break;
+            case FSMstate.Alert: UpdateAlertStatus(); break;
+            case FSMstate.Engaged: UpdateEngagedStatus(); break;
+            case FSMstate.Dead: UpdateDeadStatus(); break;
+        }
+        
+    }
+
+    protected void UpdateWaitStatus()
+    {
+        if (dead)
+        {
+            currState = FSMstate.Dead;
+        }
+        if (active)
+        {
+            
+            if (move == false & action == false)
+            {
+                active = false;
+                mapControl.SendMessage("ApplyActiveStatus", false);
+            }
+            
+            foreach (var character in playerCharacters)
+            {
+                if (Vector3.Distance(transform.position, character.transform.position) <= engagedRange)
+                {
+                    currState = FSMstate.Engaged;
+                } 
+                else if (Vector3.Distance(transform.position, character.transform.position) <= alertRange)
+                {
+                    currState = FSMstate.Alert;
+                }
+                else
+                {
+                    currState = FSMstate.Idle;
+                }
+            }
+
+        }
+    }
+
+    protected void UpdateMovingStatus()
+    {
+        // Dedicated Moving State, so the AI cannot also make an action at the same time.
+        if (dead)
+        {
+            currState = FSMstate.Dead;
+        }
+        if (move == false & action == false)
+        {
+            active = false;
+            mapControl.SendMessage("ApplyActiveStatus", false);
+        }
+
+        nav.SetDestination(destination);
+        
+        if (transform.position == destination)
+        {
+            currState = FSMstate.Wait;
+            move = false;
+        }
+    }
+
+    protected void UpdateIdleStatus()
+    {
+        if (dead)
+        {
+            currState = FSMstate.Dead;
+        }
+        if (move == false & action == false)
+        {
+            active = false;
+            mapControl.SendMessage("ApplyActiveStatus", false);
+            currState = FSMstate.Wait;
+        }
+        
+        // Move then re-assess proximity to players.
+        // destination = Random A* grid within movementRange / 4
+        //else
+        if (move)
+        {
+            destination = transform.position;
+            currState = FSMstate.Moving;
+        }
+        
+        // So that it doesn't lock trying to use abilities with no target.
+        action = false;
+    }
+
+    protected void UpdateAlertStatus()
+    {
+        if (dead)
+        {
+            currState = FSMstate.Dead;
+        }
+        if (move == false & action == false)
+        {
+            active = false;
+            mapControl.SendMessage("ApplyActiveStatus", false);
+            currState = FSMstate.Wait;
+        }
+        
+        // Move then re-assess proximity to players.
+        // destination = A* grid within movementRange / 2 towards nearest player.
+        //else
+        if (move)
+        {
+            destination = transform.position;
+            currState = FSMstate.Moving;
+        }
+        
+        // So that it doesn't lock trying to use abilities with no target.
+        action = false;
+    }
+
+    protected void UpdateEngagedStatus()
+    {
+        if (dead)
+        {
+            currState = FSMstate.Dead;
+        }
+        if (move == false & action == false)
+        {
+            active = false;
+            mapControl.SendMessage("ApplyActiveStatus", false);
+            currState = FSMstate.Wait;
+        }
+        // destination = A* grid within movementRange. Enemy type variance for attack/ability range.
+        //else
+        if (move)
+        {
+            destination = transform.position;
+            currState = FSMstate.Moving;
+        }
+        /*
+         * Action Logic
+         * Customised for individual enemy types, performing and action makes
+         */
+        action = false;
+    }
+    
+    protected void UpdateDeadStatus()
+    {
+        if (active == true)
+        {
+            active = false;
+            mapControl.SendMessage("ApplyActiveStatus", false);
+        }
+    }
+    
+    void ApplyActiveStatus(bool message)
+    {
+        active = message;
+        if (message == true)
+        {
+            action = true;
+            move = true;
+        }
+    }
+
+    void ApplyDamage(int damage)
+    {
+        currHealth -= damage;
+        if (currHealth <= 0)
+        {
+            // DEATH EFFECTS
+            mapControl.SendMessage("ApplyEnemySlain");
+            nav.isStopped = true;
+            nav.enabled = false;
+            dead = true;
+            currState = FSMstate.Dead;
+        }
+    }
+    
+}
