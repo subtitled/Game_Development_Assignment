6388b6fe9fe91d160866405f56d82cfd5fc38e0d
Thomas King <thomas.king@griffithuni.edu.au>
2020-09-25 16:36:18 +1000

    Static spawning based on team selection works.

---
+++ Assets/Scripts/spawnPointGizmos.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPointGizmos: MonoBehaviour
+{
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(new Vector3(transform.position.x, transform.position.y, transform.position.z), "wayPoint");
+    }
+}
---
+++ Assets/Scripts/spawning.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawning : MonoBehaviour
+{
+    public GameObject[] spawnPointList;
+    public GameObject[] classModels;
+
+    public int unitOne;
+    public int unitTwo;
+    public int unitThree;
+    public int unitFour;
+    public int unitFive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get value of class
+        //switch
+        //spawn depending on number
+        //do 5 times
+        unitOne = PlayerPrefs.GetInt("UnitOne");
+        unitTwo = PlayerPrefs.GetInt("UnitTwo");
+        unitThree = PlayerPrefs.GetInt("UnitThree");
+        unitFour = PlayerPrefs.GetInt("UnitFour");
+        unitFive = PlayerPrefs.GetInt("UnitFive");
+
+        switch (unitOne)
+        {
+            case 0:
+                Instantiate(classModels[0], spawnPointList[0].transform.position, Quaternion.identity);
+                break;
+            case 1:
+                Instantiate(classModels[1], spawnPointList[0].transform.position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(classModels[2], spawnPointList[0].transform.position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(classModels[3], spawnPointList[0].transform.position, Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(classModels[4], spawnPointList[0].transform.position, Quaternion.identity);
+                break;
+        }
+        switch (unitTwo)
+        {
+            case 0:
+                Instantiate(classModels[0], spawnPointList[1].transform.position, Quaternion.identity);
+                break;
+            case 1:
+                Instantiate(classModels[1], spawnPointList[1].transform.position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(classModels[2], spawnPointList[1].transform.position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(classModels[3], spawnPointList[1].transform.position, Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(classModels[4], spawnPointList[1].transform.position, Quaternion.identity);
+                break;
+        }
+        switch (unitThree)
+        {
+            case 0:
+                Instantiate(classModels[0], spawnPointList[2].transform.position, Quaternion.identity);
+                break;
+            case 1:
+                Instantiate(classModels[1], spawnPointList[2].transform.position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(classModels[2], spawnPointList[2].transform.position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(classModels[3], spawnPointList[2].transform.position, Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(classModels[4], spawnPointList[2].transform.position, Quaternion.identity);
+                break;
+        }
+        switch (unitFour)
+        {
+            case 0:
+                Instantiate(classModels[0], spawnPointList[3].transform.position, Quaternion.identity);
+                break;
+            case 1:
+                Instantiate(classModels[1], spawnPointList[3].transform.position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(classModels[2], spawnPointList[3].transform.position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(classModels[3], spawnPointList[3].transform.position, Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(classModels[4], spawnPointList[3].transform.position, Quaternion.identity);
+                break;
+        }
+        switch (unitFive)
+        {
+            case 0:
+                Instantiate(classModels[0], spawnPointList[4].transform.position, Quaternion.identity);
+                break;
+            case 1:
+                Instantiate(classModels[1], spawnPointList[4].transform.position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(classModels[2], spawnPointList[4].transform.position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(classModels[3], spawnPointList[4].transform.position, Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(classModels[4], spawnPointList[4].transform.position, Quaternion.identity);
+                break;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
