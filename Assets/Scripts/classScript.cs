9043e641e2c6744f3b50bb4ae417d2027258cf19
Thomas King <thomas.king@griffithuni.edu.au>
2020-09-26 14:58:36 +1000

    When player character spawn they spawn with reference to their class, unit number and abilities based on their unit number

--- Assets/Scripts/classScript.cs
+++ Assets/Scripts/classScript.cs
@@ -9,10 +9,10 @@ public class classScript : MonoBehaviour
 {
     //TODO: TAKE VALUE FROM ABILITY SELECTION SET ints TO VALUE FOR SWITCH STATEMENTS
     //Setup for switch statements (might change later)
-    public static int wep_1 = 0;
-    public static int wep_2 = 0;
-    public static int abil_1 = 0;
-    public static int abil_2 = 0;
+    public int wep_1;
+    public int wep_2;
+    public int abil_1;
+    public int abil_2;
 
     //Stat setup (might change later)
     public int strength;
@@ -29,6 +29,7 @@ public class classScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //generates stats for classes (change value ranges based on class)
         switch (classNum) 
         {
             case 0: //warrior
@@ -71,9 +72,40 @@ public class classScript : MonoBehaviour
                 movement = 3;
                 initiative = Random.Range(1, 11) + agility;
                 break;
-
         }
-        //generates stats for classes (change value ranges based on class)
+        switch (unitNum)
+        {
+            case 0:
+                wep_1 = PlayerPrefs.GetInt("U1W1");
+                wep_2 = PlayerPrefs.GetInt("U1W2");
+                abil_1 = PlayerPrefs.GetInt("U1A1");
+                abil_2 = PlayerPrefs.GetInt("U1A2");
+                break;
+            case 1:
+                wep_1 = PlayerPrefs.GetInt("U2W1");
+                wep_2 = PlayerPrefs.GetInt("U2W2");
+                abil_1 = PlayerPrefs.GetInt("U2A1");
+                abil_2 = PlayerPrefs.GetInt("U2A2");
+                break;
+            case 2:
+                wep_1 = PlayerPrefs.GetInt("U3W1");
+                wep_2 = PlayerPrefs.GetInt("U3W2");
+                abil_1 = PlayerPrefs.GetInt("U3A1");
+                abil_2 = PlayerPrefs.GetInt("U3A2");
+                break;
+            case 3:
+                wep_1 = PlayerPrefs.GetInt("U4W1");
+                wep_2 = PlayerPrefs.GetInt("U4W2");
+                abil_1 = PlayerPrefs.GetInt("U4A1");
+                abil_2 = PlayerPrefs.GetInt("U4A2");
+                break;
+            case 4:
+                wep_1 = PlayerPrefs.GetInt("U5W1");
+                wep_2 = PlayerPrefs.GetInt("U5W2");
+                abil_1 = PlayerPrefs.GetInt("U5A1");
+                abil_2 = PlayerPrefs.GetInt("U5A2");
+                break;
+        }
 
         
     }
--- Assets/Scripts/spawning.cs
+++ Assets/Scripts/spawning.cs
@@ -29,90 +29,115 @@ public class spawning : MonoBehaviour
         switch (unitOne)
         {
             case 0:
+                classModels[0].GetComponent<classScript>().unitNum = 0;
                 Instantiate(classModels[0], spawnPointList[0].transform.position, Quaternion.identity);
                 break;
             case 1:
+                classModels[1].GetComponent<classScript>().unitNum = 0;
                 Instantiate(classModels[1], spawnPointList[0].transform.position, Quaternion.identity);
                 break;
             case 2:
+                classModels[2].GetComponent<classScript>().unitNum = 0;
                 Instantiate(classModels[2], spawnPointList[0].transform.position, Quaternion.identity);
                 break;
             case 3:
+                classModels[3].GetComponent<classScript>().unitNum = 0;
                 Instantiate(classModels[3], spawnPointList[0].transform.position, Quaternion.identity);
                 break;
             case 4:
+                classModels[4].GetComponent<classScript>().unitNum = 0;
                 Instantiate(classModels[4], spawnPointList[0].transform.position, Quaternion.identity);
                 break;
         }
         switch (unitTwo)
         {
             case 0:
+                classModels[0].GetComponent<classScript>().unitNum = 1;
                 Instantiate(classModels[0], spawnPointList[1].transform.position, Quaternion.identity);
                 break;
             case 1:
+                classModels[1].GetComponent<classScript>().unitNum = 1;
                 Instantiate(classModels[1], spawnPointList[1].transform.position, Quaternion.identity);
                 break;
             case 2:
+                classModels[2].GetComponent<classScript>().unitNum = 1;
                 Instantiate(classModels[2], spawnPointList[1].transform.position, Quaternion.identity);
                 break;
             case 3:
+                classModels[3].GetComponent<classScript>().unitNum = 1;
                 Instantiate(classModels[3], spawnPointList[1].transform.position, Quaternion.identity);
                 break;
             case 4:
+                classModels[4].GetComponent<classScript>().unitNum = 1;
                 Instantiate(classModels[4], spawnPointList[1].transform.position, Quaternion.identity);
                 break;
         }
         switch (unitThree)
         {
             case 0:
+                classModels[0].GetComponent<classScript>().unitNum = 2;
                 Instantiate(classModels[0], spawnPointList[2].transform.position, Quaternion.identity);
                 break;
             case 1:
+                classModels[1].GetComponent<classScript>().unitNum = 2;
                 Instantiate(classModels[1], spawnPointList[2].transform.position, Quaternion.identity);
                 break;
             case 2:
+                classModels[2].GetComponent<classScript>().unitNum = 2;
                 Instantiate(classModels[2], spawnPointList[2].transform.position, Quaternion.identity);
                 break;
             case 3:
+                classModels[3].GetComponent<classScript>().unitNum = 2;
                 Instantiate(classModels[3], spawnPointList[2].transform.position, Quaternion.identity);
                 break;
             case 4:
+                classModels[4].GetComponent<classScript>().unitNum = 2;
                 Instantiate(classModels[4], spawnPointList[2].transform.position, Quaternion.identity);
                 break;
         }
         switch (unitFour)
         {
             case 0:
+                classModels[0].GetComponent<classScript>().unitNum = 3;
                 Instantiate(classModels[0], spawnPointList[3].transform.position, Quaternion.identity);
                 break;
             case 1:
+                classModels[1].GetComponent<classScript>().unitNum = 3;
                 Instantiate(classModels[1], spawnPointList[3].transform.position, Quaternion.identity);
                 break;
             case 2:
+                classModels[2].GetComponent<classScript>().unitNum = 3;
                 Instantiate(classModels[2], spawnPointList[3].transform.position, Quaternion.identity);
                 break;
             case 3:
+                classModels[3].GetComponent<classScript>().unitNum = 3;
                 Instantiate(classModels[3], spawnPointList[3].transform.position, Quaternion.identity);
                 break;
             case 4:
+                classModels[4].GetComponent<classScript>().unitNum = 3;
                 Instantiate(classModels[4], spawnPointList[3].transform.position, Quaternion.identity);
                 break;
         }
         switch (unitFive)
         {
             case 0:
+                classModels[0].GetComponent<classScript>().unitNum = 4;
                 Instantiate(classModels[0], spawnPointList[4].transform.position, Quaternion.identity);
                 break;
             case 1:
+                classModels[1].GetComponent<classScript>().unitNum = 4;
                 Instantiate(classModels[1], spawnPointList[4].transform.position, Quaternion.identity);
                 break;
             case 2:
+                classModels[2].GetComponent<classScript>().unitNum = 4;
                 Instantiate(classModels[2], spawnPointList[4].transform.position, Quaternion.identity);
                 break;
             case 3:
+                classModels[3].GetComponent<classScript>().unitNum = 4;
                 Instantiate(classModels[3], spawnPointList[4].transform.position, Quaternion.identity);
                 break;
             case 4:
+                classModels[4].GetComponent<classScript>().unitNum = 4;
                 Instantiate(classModels[4], spawnPointList[4].transform.position, Quaternion.identity);
                 break;
         }
