1b5fcb3015cedba0a9174fa5742c9865845c2eda
Thomas King <thomas.king@griffithuni.edu.au>
2020-09-28 12:25:33 +1000

    added enemy models and spawn points (static, not randomised)

---
+++ Assets/Scripts/enemySpawning.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawning : MonoBehaviour
+{
+    public GameObject[] spawnPointList;
+    public GameObject[] enemyModels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(enemyModels[0], spawnPointList[0].transform.position, Quaternion.Euler(0f, 180f, 0f));
+        Instantiate(enemyModels[1], spawnPointList[1].transform.position, Quaternion.Euler(0f, 180f, 0f));
+        Instantiate(enemyModels[2], spawnPointList[2].transform.position, Quaternion.Euler(0f, 180f, 0f));
+        Instantiate(enemyModels[3], spawnPointList[3].transform.position, Quaternion.Euler(0f, 180f, 0f));
+        Instantiate(enemyModels[4], spawnPointList[4].transform.position, Quaternion.Euler(0f, 180f, 0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
