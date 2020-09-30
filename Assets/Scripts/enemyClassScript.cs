b39b1b75dc6db348641c7afc7e6d3ffb59febf36
Thomas King <thomas.king@griffithuni.edu.au>
2020-09-30 16:48:07 +1000

    Changed enemy prefabs, created enemy stat sheet, will start adding placeholder damage values for possible attacks/abilities

---
+++ Assets/Scripts/enemyClassScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyClassScript : MonoBehaviour
+{
+
+    //Stat setup (might change later)
+    public int strength;
+    public int agility;
+    public int intelligence;
+    public int health;
+    public int movement;
+    public int initiative; //change depending on class
+
+    //extra setup for referencing
+    public int enemyClassNum;
+    public int unitNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //generates stats for classes (change value ranges based on class)
+        switch (enemyClassNum)
+        {
+            case 0: //enemyWarrior
+                strength = Random.Range(7, 9);
+                agility = Random.Range(5, 7);
+                intelligence = Random.Range(2, 5);
+                health = Random.Range(110, 130);
+                movement = 3;
+                initiative = Random.Range(1, 11) + agility;
+                break;
+            case 1: //enemyPriest
+                strength = Random.Range(5, 7);
+                agility = Random.Range(5, 7);
+                intelligence = Random.Range(6, 8);
+                health = Random.Range(100, 120);
+                movement = 3;
+                initiative = Random.Range(1, 11) + agility;
+                break;
+            case 2: //enemyMage
+                strength = Random.Range(2, 5);
+                agility = Random.Range(6, 8);
+                intelligence = Random.Range(7, 9);
+                health = Random.Range(90, 110);
+                movement = 3;
+                initiative = Random.Range(1, 11) + agility;
+                break;
+            case 3: //enemyRogue
+                strength = Random.Range(6, 8);
+                agility = Random.Range(7, 9);
+                intelligence = Random.Range(5, 6);
+                health = Random.Range(100, 110);
+                movement = 3;
+                initiative = Random.Range(1, 11) + agility;
+                break;
+            case 4: //enemyMarksman
+                strength = Random.Range(2, 5);
+                agility = Random.Range(6, 8);
+                intelligence = Random.Range(2, 5);
+                health = Random.Range(90, 110);
+                movement = 3;
+                initiative = Random.Range(1, 11) + agility;
+                break;
+        }
+    }
+}
