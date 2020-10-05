26911673166a80ee3769a8c42eda3a9c7600cf67
Thomas King <thomas.king@griffithuni.edu.au>
2020-10-05 15:02:47 +1000

    cameras are switchable

---
+++ Assets/Scripts/cameraSwitch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cameraSwitch : MonoBehaviour
+{
+    public Camera[] cameras;
+    public void Start()
+    {
+        cameras[0].enabled = true;
+        cameras[1].enabled = false;
+        cameras[2].enabled = false;
+        cameras[3].enabled = false;
+    }
+
+    public void switchLeftCamera()
+    {
+        cameras[0].enabled = false;
+        cameras[1].enabled = true;
+        cameras[2].enabled = false;
+        cameras[3].enabled = false;
+    }
+
+    public void switchFrontCamera() 
+    {
+        cameras[0].enabled = true;
+        cameras[1].enabled = false;
+        cameras[2].enabled = false;
+        cameras[3].enabled = false;
+    }
+
+    public void switchBackCamera()
+    {
+        cameras[0].enabled = false;
+        cameras[1].enabled = false;
+        cameras[2].enabled = true;
+        cameras[3].enabled = false;
+    }
+
+    public void switchRightCamera()
+    {
+        cameras[0].enabled = false;
+        cameras[1].enabled = false;
+        cameras[2].enabled = false;
+        cameras[3].enabled = true;
+    }
+}
